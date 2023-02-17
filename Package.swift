0d3b8e343cc2cdfdca8ab64ddf078e96aeab42ec
Daniel Kennett <daniel@cascable.se>
2023-02-17 10:17:57 +0100

    Add SPM release for 4.4

---
+++ Package.swift
@@ -0,0 +1,10 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "Paddle",
+    platforms: [.macOS(.v10_13)],
+    products: [.library(name: "Paddle", targets: ["Paddle"])],
+    dependencies: [],
+    targets: [.binaryTarget(name: "Paddle", path: "Paddle.xcframework")]
+)
